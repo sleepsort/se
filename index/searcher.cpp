7f0f5daaff46a74282cd80ad7688e70c299dc346
Han Jiang <jianghan08@gmail.com>
2013-03-10 22:09:40 +0800

    support near & phrase query

--- index/searcher.cpp
+++ index/searcher.cpp
@@ -67,15 +67,110 @@ void IndexSearcher::searchNOT(Query *q) {
 }
 
 void IndexSearcher::searchPHRSE(Query *q) {
+    vector<int> hits;
+
+    disjunct(hits, q->get(0)->docs(), hits);
+    for (unsigned i=1; i<q->size(); i++)
+        conjunct(hits, q->get(i)->docs(), hits);
+
+    if (q->size() == 1) {
+        q->docs().insert(q->docs().begin(), hits.begin(), hits.end());
+        return;
+    }
+
+    vector<int> phrase; 
+    bool success = true;
+
+    for (unsigned i=0; i<q->size(); i++) {
+        string t= q->get(i)->token;
+        int tid;
+        transform(t.begin(), t.end(), t.begin(), ::tolower);
+        if (termmap.find(t) == termmap.end()) {
+            return;
+        }
+        tid = termmap[t];
+        phrase.push_back(tid);
+    }
+    for (unsigned k=0; k<hits.size(); k++) {
+        int did = hits[k];
+        vector<vector<int>*> positions; 
+        vector<unsigned> upto; 
+        for (unsigned i=0; i<phrase.size(); i++) {
+            int tid = phrase[i];
+            positions.push_back(&postings[tid][did]);
+            upto.push_back(0);
+        }
+        vector<int> &pos0 = (*positions[0]);
+        while (upto[0] < pos0.size()) {
+            success = true;
+            for (unsigned i=1; i<phrase.size(); i++) {
+                vector<int> &posi = (*positions[i]);
+                while (upto[i] < posi.size() && 
+                       posi[upto[i]] < pos0[upto[0]] + int(i)) {
+                    upto[i]++;
+                }
+                if (upto[i] == posi.size()) {
+                    success = false;
+                    break;
+                }
+                if (posi[upto[i]] != pos0[upto[0]] + int(i)) {
+                    success = false;
+                }
+            }
+            if (success)
+                break;
+            upto[0]++;
+        }
+        if (success) {
+            q->docs().push_back(did);
+        }
+    }
 }
 
 void IndexSearcher::searchNEAR(Query *q) {
+    int offset = atoi(q->info.c_str());
+    vector<int> hits;
+    vector<int> phrase; 
+
+    disjunct(hits, q->get(0)->docs(), hits);
+    conjunct(hits, q->get(1)->docs(), hits);
+
+    for (unsigned i=0; i<q->size(); i++) {
+        string t= q->get(i)->token;
+        transform(t.begin(), t.end(), t.begin(), ::tolower);
+        if (termmap.find(t) == termmap.end()) {
+            return;
+        }
+        phrase.push_back(termmap[t]);
+    }
+    for (unsigned i=0; i<hits.size(); i++) {
+        vector<int> &v0=postings[phrase[0]][hits[i]];
+        vector<int> &v1=postings[phrase[1]][hits[i]];
+        unsigned s0 = 0, s1 = 0;
+        int pos0, pos1, dist;
+        bool success = false;
+        while (s0 < v0.size() && s1 < v1.size()) {
+            pos0 = v0[s0], pos1 = v1[s1];
+            dist = pos0 - pos1;
+            if (dist > 0) 
+                s1++;
+            else
+                s0++;
+            dist = dist > 0 ? dist : -dist;
+            if (dist <= offset) {
+               success = true;
+               break;
+            }
+        }
+        if (success)
+            q->docs().push_back(hits[i]);
+    }
 }
 
 
 void IndexSearcher::report(Query* q) {
-    cout << "query = " << q->token << endl;
-    cout << "numhit = " << q->docs().size() << endl;
+cout << "query = " << q->token << endl;
+cout << "numhit = " << q->docs().size() << endl;
     cout << "hitdocs = " << endl;
     for (unsigned i=0; i<q->docs().size(); i++) {
         int did = q->docs()[i];
