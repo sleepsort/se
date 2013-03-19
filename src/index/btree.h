70fd5f860cfe724f635617d5061ad6796e786e5c
Han Jiang <jianghan08@gmail.com>
2013-03-19 19:03:01 +0800

    add btree and its utility, however it only support int

---
+++ src/index/btree.h
@@ -0,0 +1,103 @@
+#ifndef INDEX_BTREE_H_
+#define INDEX_BTREE_H_
+
+#include <stdio.h>
+#include <assert.h>
+#include <memory.h>
+#include <string>
+#include <iostream>
+#include <map>
+#include "util/util.h"
+using namespace std;
+
+// In-memory and in-disk format of b-tree node
+// NOTE: this structure should always be 
+// byte-aligned
+class BNode {
+ public:
+  static const int MIN_DEGREE = 3;
+  static const int MAX_DEGREE = MIN_DEGREE * 2 - 1;
+  static const int HALF = MAX_DEGREE / 2;
+  BNode();
+  ~BNode();
+ 
+ public:
+  int id();
+  void set(int nid);
+  void clear();
+
+ public:
+  int keys[MAX_DEGREE + 1];
+  int next[MAX_DEGREE + 2];
+  int numkeys;
+  int leaf;
+
+ private:
+  int my_id;
+};
+
+// Memory scheduler, maintaining a memory pool for b-tree nodes, 
+// every time the caller gets one page, it is responsible to return 
+// the page back to manager.
+// every time the caller modifies one page, it should also tell
+// manager to update it.
+class BManager {
+  public:
+    static const int MEMORY_BUFF = 4;
+    static const int NODE_SZ = sizeof(BNode);
+    BManager();
+    ~BManager();
+    void init(string &meta_path, string &data_path);
+    BNode* new_node();
+    BNode* new_root();
+    BNode* get_node(int nodeid);
+    BNode* get_root();
+    void update_node(int nodeid);
+    void return_node(int nodeid);
+    void dump();
+
+  private:
+    int allocate();
+    int filepos(int nodeid);
+    void flush(int nodeid);
+    void load(int nodeid);
+
+  private:
+    string meta_path;
+    string data_path;
+    FILE* meta_file;
+    FILE* data_file;
+    int root_node_id;
+    int num_nodes;
+    map<int, int> nodemap;        // node id => page id
+    BNode pool[MEMORY_BUFF];
+    int bitmap[MEMORY_BUFF];
+};
+
+// The tree only maintains the root node, other nodes are
+// requested from BManager
+class BTree {
+ public:
+  BTree(string &metapath, string &datapath);
+  ~BTree();
+  void insert(int key);
+  BNode* walk(int key);
+  BNode* search(int key);
+  BNode* get(int nodeid);
+  void free(int nodeid);
+  void update(int nodeid);
+  void dump();
+  void dump(BNode*);
+  void dumpN(BNode*);
+  void sort();
+  void sort(BNode*);
+
+ private:
+  BNode* split(BNode* cur);
+
+ private:
+  BManager manager;
+  BNode *root;
+};
+
+#endif  // INDEX_BTREE_H_
--- src/util/util.cpp
+++ src/util/util.cpp
@@ -200,3 +200,61 @@ void dump(vector<pair<int, int> > &a) {
   }
   cout << endl;
 }
+
+
+
+// insert key to fit strict ascending order
+int array_insert(int* a, int len, int key) {
+  if (len == 0) {
+    a[0] = key;
+    return 0;
+  }
+  int i = 0, j = 0;
+  j = i = bsearch(a, len, key);
+  if (i < len && a[i] == key) {   // duplicate
+    return -1;
+  }
+  j = len;
+  while (j > i) {
+    a[j] = a[j-1];
+    j--;
+  }
+  a[i] = key;
+  return i;
+}
+
+// insert key in fixed position
+// the array is unordered
+void array_insert(int* a, int len, int key, int pos) {
+  int j = len;
+  if (a[pos] == key)
+    return;
+  while (j > pos) {
+    a[j] = a[j-1];
+    j--;
+  }
+  a[pos] = key;
+}
+
+
+// binary search, return the position
+// of found key, or appropriate position
+// to insert while keeping ascending order
+int bsearch(const int*a, int len, int key) {
+  int f = 0, t = len -1, m = 0;
+  while (f <= t) {
+    m = (f+t)/2;
+    if (a[m] == key) {
+      return m;
+    } else if (a[m] < key) {
+      f = m+1;
+    } else if (a[m] > key) {
+      t = m-1;
+    }
+  }
+  if (a[m] < key) {
+    return m+1;
+  } else {
+    return m;
+  }
+}
