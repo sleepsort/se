6ee63904755537fd76a6f9a02e545b06415f04f4
Han Jiang <jianghan08@gmail.com>
2013-05-03 11:16:08 +0800

    add file loader, untest

---
+++ src/index/loader.h
@@ -0,0 +1,48 @@
+#ifndef INDEX_LOADER_H_
+#define INDEX_LOADER_H_
+#include <sys/stat.h>
+#include <cassert>
+#include <fstream>
+#include "index/writer.h"
+#include "util/string.h"
+#include "util/file.h"
+#include "util/xml.h"
+using namespace std;
+
+enum Corpus {
+    CORPUS_SHAKES,
+    CORPUS_RCV1,
+    CORPUS_GOV2,
+    CORPUS_RAW
+};
+
+class FileLoader {
+ public:
+  FileLoader(const string &dir, Corpus type = CORPUS_RAW);
+  ~FileLoader();
+
+  void init();
+
+  bool next();
+  void attr(DocAttr &attr);
+  void words(vector<string> &words);
+
+ private:
+  void parseRCV1();
+  void parseGOV2();
+  void parseRAW();
+
+
+ private:
+  string dir;
+  Corpus type;
+
+  ifstream fin;
+  vector<string> files;
+  unsigned upto;
+
+  vector<string> m_words;
+  map<string, string> m_content;
+};
+
+#endif  // INDEX_LOADER_H_
